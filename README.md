8daa95d3519f95ae5585c75c15875716e25cb55c
Rob <robbcardenas@gmail.com>
2015-06-29 21:28:13 -0700

    Created an index file with its corresponding js and css files.

---
+++ README.md
@@ -0,0 +1,7 @@
+#README
+
+This is a Todo List app in which a user is able to:
+
+-Add things to do to the list
+-See the items that are currently on their list
+-Check off items from their list
---
+++ script.js
@@ -0,0 +1,39 @@
+$(function() {
+
+	var $newItemForm = $("#new_todo_item");
+
+	var $taskOL = $("#todo_list");
+
+	$newItemForm.on("submit", function(event) {
+		event.preventDefault();
+		console.log($( "#item_name" ).val());
+		var $newLI = $('<li class="item">' + $("#item_name").val() + '</li>');
+		$("#todo_list").append($newLI);
+
+		//add new item to the DOM
+		todoList.push({
+			title: $("#item_name").val(),
+		});
+	});
+
+	var todoList = [];
+
+	var item1 = {title: "Kick Ass",
+				description: "World Domination",
+				due_date: "Past Due"
+				};
+
+	todoList.push(item1);
+
+	// Completed tasks get crossed out
+	$taskOL.on("click", ".item", function(event) {
+		$(this).addClass("complete");
+	});
+
+	// After task gets crossed, the item list disappears
+	$taskOL.on("click", ".item", function(event) {
+  		$(this).hide("slow");
+  	});
+
+});
+
