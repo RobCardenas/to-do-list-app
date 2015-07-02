dd99446eb4dbd9c2118a2a2e0b31ae9c36605bcd
Rob <robbcardenas@gmail.com>
2015-07-01 21:36:20 -0700

    refactored using function constructor.

--- script.js
+++ script.js
@@ -1,61 +1,71 @@
 $(function() {
 
-	// new to do list
-	var $newItemForm = $("#new_todo_item");
+    // new to do list
+    var $newItemForm = $("#new_todo_item");
 
-	// variable that stores our to do list
-	var $taskOL = $("#todo_list");
+    // variable that stores our to do list
+    var $taskOL = $("#todo_list");
 
-	// todo template
-	var toDoTemplate = _.template($('#todo-template').html());
+    // todo template
+    var toDoTemplate = _.template($('#todo-template').html());
 
-	// `todoList` array is our model (holds our data)
-  // contains test (or "seed") data
-  var todoList = [
-    {task: "Kick Some Ass", description: "Use JavaScript"},
-    {task: "World Domination", description: "Become Friends with Elon Musk"},
-    {task: "End Poverty", description: "Build app, sell it, profit, donate!"}
-  ];
+    function ToDo(task,description) {
+    this.task = task;
+    this.description = description;
+    }
 
-  // append existing todos (from seed data) to `$taskOL`
-  // `_.each` is an "iterator" function provided by Underscore.js
-  _.each(todoList, function (todo, index) {
+    ToDo.all = [];
+
+    var task1 = new ToDo("Kick Ass", "Use JavaScript");
+    var task2 = new ToDo("World Domination", "Befriend Elon Musk");
+
+    ToDo.prototype.save = function(){
+    // add tasks to ToDo array
+    ToDo.all.push(this);
+    };
+
+    task1.save();
+    task2.save();
+
+    // Function that renders tasks into ToDo array
+    ToDo.prototype.render = function() {
+    return toDoTemplate;
+    };
+
+    // appends existing todos (from seed data) to `$taskOL`
+    _.each(ToDo.all, function (todo, index) {
     var $todo = $(toDoTemplate(todo));
     $todo.attr('data-index', index);
     $taskOL.append($todo);
-  });
+    });
 
-	$newItemForm.on("submit", function(event) {
-		event.preventDefault();
+    // on click to submit our data
+    $newItemForm.on("submit", function(event) {
+    event.preventDefault();
 
-		// create new todo object from form data
+    // create new todo object from form data
     var toDoName = $('#item_name').val();
     var toDoDesc = $('#item_descr').val();
-    var toDoData = {task: toDoName, description: toDoDesc};
+    var newTask = new ToDo(toDoName, toDoDesc);
 
-		// var $newLI = $('<li class="item">' + $("#item_name").val() + ' &nbsp;&#124;&nbsp; ' + $("#item_descr").val() + '</li>');
-		// $("#todo_list").append($newLI);
-
-		//add new item to the DOM
-		todoList.push(toDoData);
-    console.log(todoList);
-    var index = todoList.indexOf(toDoData);
+    newTask.save();
 
     // append our new todo to the page
-    var $todo = $(toDoTemplate(toDoData));
-    $todo.attr('data-index', index);
+    var $todo = $(toDoTemplate(newTask));
+
+    $todo.attr(newTask.task, newTask.description);
     $taskOL.append($todo);
 
-		// reset the form
+    // reset the form
     $newItemForm[0].reset();
     $("#item-name").focus();
 
-	});
+  });
 
-		// Completed tasks get crossed out and disappear using method chaining
-		$taskOL.on("click", ".item", function() {
-		$(this).addClass("complete").hide("slow");
-	});
+    // Completed tasks get crossed out and disappear using method chaining
+    $taskOL.on("click", ".item", function() {
+    $(this).addClass("complete").hide("fast");
+    });
+    console.log("That's a bingo!");
 
-});
-
+  });
